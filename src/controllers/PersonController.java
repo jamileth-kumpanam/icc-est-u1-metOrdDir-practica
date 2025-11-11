a6169cc6550f2bdfc2c919aeeb8c2f3ba6a42777
jamileth-kumpanam <jamileth.kump4@gmail.com>
2025-11-11 18:59:51 +0000

    Docker

--- src/controllers/PersonController.java
+++ src/controllers/PersonController.java
@@ -4,22 +4,21 @@ import models.Person;
 
 public class PersonController {
 
-    public void sortByName(Person[] personas){
+    public void sortByName(Person[] personas) {
         int n = personas.length;
-        for (int i = 0; i < n-1; i++) {
+        for (int i = 0; i < n - 1; i++) {
             boolean swapped = false;
-            for (int j = 0; j < n-i-1; j++) {
-                if (personas[j].getName().compareToIgnoreCase(personas[j+1].getName()) > 0) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (personas[j].getName().compareToIgnoreCase(personas[j + 1].getName()) > 0) {
                     Person temp = personas[j];
-                    personas[j] = personas[j+1];
-                    personas[j+1] = temp;
+                    personas[j] = personas[j + 1];
+                    personas[j + 1] = temp;
                     swapped = true;
                 }
             }
-            if(!swapped){
+            if (!swapped)
                 break;
-            }
         }
     }
-    
+
 }
